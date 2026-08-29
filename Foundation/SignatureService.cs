2ca22b4e75dc3d6d8a572c409a96262cec1e6c03
Rafael Bergo <rafaelbergo503@gmail.com>
2026-08-28 23:35:31 -0300

    feat: Add service to sign strings and verify

---
+++ Foundation/SignatureService.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Foundation;
+
+public class SignatureService
+{
+    public string SignText(string text, string privateKeyPath)
+    {
+        using var rsa = RSA.Create();
+        rsa.ImportFromPem(File.ReadAllText(privateKeyPath));
+        
+        byte[] data = Encoding.UTF8.GetBytes(text);
+        byte[] signature = rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+ 
+        return Convert.ToBase64String(signature);
+    }
+
+    public bool VerifySignature(string text, string signature, string publicKeyPath)
+    {
+        using var rsa = RSA.Create();
+        rsa.ImportFromPem(File.ReadAllText(publicKeyPath));
+        
+        byte[] data = Encoding.UTF8.GetBytes(text);
+        byte[] signatureBytes = Convert.FromBase64String(signature);
+        
+        return rsa.VerifyData(data, signatureBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+    }
+}
--- MS.Principal/Program.cs
+++ MS.Principal/Program.cs
@@ -1,7 +1,19 @@
-﻿using Foundation.Keys;
+﻿using Foundation;
+using Foundation.Keys;
 
 string solutionRootPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
 var keyManagement = new KeyManagement(solutionRootPath, "MS.Principal");
+var signature = new SignatureService();
 
 keyManagement.CheckKeys();
 
+// Test sign and verify text
+string text = "Test message";
+string privateKeyPath = Path.Combine(solutionRootPath, "MS.Principal", "Keys", "MS.Principal.private.pem");
+string publicKeyPath = Path.Combine(solutionRootPath, "MS.Principal", "Keys", "MS.Principal.public.pem");
+
+string validateResult = signature.SignText(text, privateKeyPath);
+Console.WriteLine(validateResult);
+
+bool isVerified = signature.VerifySignature(text, validateResult, publicKeyPath);
+Console.WriteLine($"Valid: {isVerified}");
